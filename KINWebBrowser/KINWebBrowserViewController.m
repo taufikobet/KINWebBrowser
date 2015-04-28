b8849524253e7740291bd16cc0f4bce1a6367bc3
Taufik Obet <obet@happy5.co>
2015-04-28 13:37:23 +0700

    Set bar button item tint color

--- KINWebBrowser/KINWebBrowserViewController.m
+++ KINWebBrowser/KINWebBrowserViewController.m
@@ -160,6 +160,7 @@ static void *KINContext = &KINContext;
     [UINavigationBar appearance].barTintColor = [UIColor colorWithHue:0.566 saturation:0.762 brightness:0.858 alpha:1];
     NSDictionary *textTitleOptions = [NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], UITextAttributeTextColor, [UIFont fontWithName:@"AvenirNext-Medium" size:19.0f], NSFontAttributeName, nil];
     [[UINavigationBar appearance] setTitleTextAttributes:textTitleOptions];
+    [UIBarButtonItem appearance] setTintColor:[UIColor whiteColor]];
     
 }
 
@@ -435,6 +436,7 @@ static void *KINContext = &KINContext;
                 [UINavigationBar appearance].barTintColor = [UIColor whiteColor];
                 NSDictionary *textTitleOptions = [NSDictionary dictionaryWithObjectsAndKeys:[UIColor blackColor], UITextAttributeTextColor, nil];
                 [[UINavigationBar appearance] setTitleTextAttributes:textTitleOptions];
+                [[UIBarButtonItem appearance] setTintColor:[UIColor blueColor]];
             }];
         }
     });
